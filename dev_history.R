6c85829889ec15ebe7e27c739a440f8a35760ad8
ptaconet <paul.taconet@ird.fr>
2020-01-11 19:39:19 +0000

    Initial commit

---
+++ dev_history.R
@@ -0,0 +1,10 @@
+require(usethis)
+require(devtools)
+require(attachment)
+require(pkgdown)
+## See tutorials here : https://rtask.thinkr.fr/blog/rmd-first-when-development-starts-with-documentation/
+## and https://usethis.r-lib.org/articles/articles/usethis-setup.html
+## and video here : http://www.user2019.fr/static/pres/t257651.zip
+
+## create the package
+usethis::create_package("/home/ptaconet/getRemoteData")
