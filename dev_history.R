9c20972e66b42035c49456fa4c1a5c9b8d56545f
ptaconet <paul.taconet@ird.fr>
2020-01-11 19:43:27 +0000

    initial commit

--- dev_history.R
+++ dev_history.R
@@ -8,3 +8,22 @@ require(pkgdown)
 
 ## create the package
 usethis::create_package("/home/ptaconet/getRemoteData")
+## Manual step : Create a dev_history.R file that archives all the package history steps. Then copy it to the package folder.
+## Then proceed :
+usethis::use_build_ignore("dev_history.R")
+usethis::use_git()
+usethis::use_gpl3_license()
+devtools::check()
+usethis::proj_get()
+
+## Manual steps : If not installed, install these packages
+# system("brew install libssh2")
+# system("brew install libgit2")
+# install.packages("git2r")
+
+## Manual step : Fill-in DESCRIPTION file with title and description of the package
+## Manual step : If not already done, add a local SSH key following the instructions here : https://help.github.com/en/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
+## Then proceed :
+usethis::use_github()
+devtools::install()
+usethis::use_readme_rmd()
