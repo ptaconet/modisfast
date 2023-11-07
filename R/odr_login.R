7c1ac0e43d1d9b40e798398ec322b69258195622
babakou9 <66430862+babakou9@users.noreply.github.com>
2023-11-08 00:27:22 +0100

    Update odr_login.R
    Changed connection test url to : https://opendap.cr.usgs.gov/opendap/hyrax/MOD11_L2.061/MOD11_L2.061.ncml

--- R/odr_login.R
+++ R/odr_login.R
@@ -32,7 +32,7 @@ odr_login<-function(credentials,source,verbose=TRUE){
   if(verbose){cat("Checking credentials...\n")}
 
   if(source=="earthdata"){
-   x <- httr::GET(url = "https://opendap.cr.usgs.gov/opendap/hyrax/MOD11A2.006/h17v07.ncml.ascii?time",httr::authenticate(user=credentials[1], credentials[2]),config = list(maxredirs=-1)) # testing credentials must be improved...
+   x <- httr::GET(url = "https://opendap.cr.usgs.gov/opendap/hyrax/MOD11_L2.061/MOD11_L2.061.ncml.ascii?time",httr::authenticate(user=credentials[1], credentials[2]),config = list(maxredirs=-1)) # testing credentials must be improved...
    httr::stop_for_status(x, "login to Earthdata. Check out username and password")
    httr::warn_for_status(x)
    options(earthdata_user=credentials[1])
